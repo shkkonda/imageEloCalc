59799c198fca865910be736d849d0eeffbad313f
Shriharsha Konda <shkkonda@gmail.com>
2023-04-16 15:11:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import random
 import streamlit as st
 import pandas as pd
+from streamlit_image_select import image_select
 
 IMAGE_FOLDER = "nokiamon_list/"
 CSV_URL = "https://raw.githubusercontent.com/shkkonda/imageEloCalc/main/responses.csv"
@@ -15,28 +16,15 @@ def get_random_images():
     return image1, image2
 
 def main():
+    st.set_page_config(page_title="Image Preference Survey")
+
     st.title("Image Preference Survey")
     st.write("Please select which image you prefer out of the two options shown.")
 
     image1, image2 = get_random_images()
 
-    col1, col2 = st.beta_columns(2)
-    with col1:
-        if st.button("", key=f"select_{image1}"):
-            selected_image = image1
-        st.image(os.path.join(IMAGE_FOLDER, image1), use_column_width=True)
-    with col2:
-        if st.button("", key=f"select_{image2}"):
-            selected_image = image2
-        st.image(os.path.join(IMAGE_FOLDER, image2), use_column_width=True)
-
-    if st.button("Submit"):
-        try:
-            selected_image
-        except NameError:
-            st.warning("Please select an image.")
-            return
-
+    selected_image = image_select([os.path.join(IMAGE_FOLDER, image1), os.path.join(IMAGE_FOLDER, image2)])
+    if selected_image:
         # Store the response in a CSV file on GitHub
         data = {"Image 1": os.path.join(IMAGE_FOLDER, image1), "Image 2": os.path.join(IMAGE_FOLDER, image2), "Choice": selected_image}
         df = pd.DataFrame(data, index=[0])
@@ -48,7 +36,6 @@ def main():
             st.success("Response submitted successfully.")
         except Exception as e:
             st.error("Failed to submit response. Error: " + str(e))
-            
-            
+
 if __name__ == "__main__":
     main()
