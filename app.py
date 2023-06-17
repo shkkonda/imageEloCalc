99b6c18618c136a7e53674405a7da3b2a23d5d47
Shriharsha Konda <shkkonda@gmail.com>
2023-06-17 22:09:12 +0530

    image button

--- app.py
+++ app.py
@@ -18,11 +18,13 @@ def get_random_image_pair(df) -> Tuple[str, str]:
 def show_image_pair(left_image: str, right_image: str):
     col1, col2 = st.columns(2)
     with col1:
-        if st.button(label=f'<img src="{left_image}" width="300">', key="left_button", unsafe_allow_html=True):
+        if st.button(label="Left Image"):
             pass  # Placeholder action for left button press
+        st.markdown(f'<img src="{left_image}" width="300">', unsafe_allow_html=True)
     with col2:
-        if st.button(label=f'<img src="{right_image}" width="300">', key="right_button", unsafe_allow_html=True):
+        if st.button(label="Right Image"):
             pass  # Placeholder action for right button press
+        st.markdown(f'<img src="{right_image}" width="300">', unsafe_allow_html=True)
 
 def main(df):
     st.title("Nokiamon ELO Rating")
