5aa7f6d6758d28ee88011a4a797a30109d1b5f76
Shriharsha Konda <shkkonda@gmail.com>
2023-06-21 00:10:32 +0530

    save selections

--- app.py
+++ app.py
@@ -16,8 +16,21 @@ port = '5432'
 password = 'wCJTQ205EKHWh6fXzxLc'
 
 # Connect to the database
-# conn = psycopg2.connect(host=host, dbname=dbname, user=user, port=port, password=password)
-# cur = conn.cursor()
+conn = psycopg2.connect(host=host, dbname=dbname, user=user, port=port, password=password)
+cur = conn.cursor()
+
+# Create user_selections table if it doesn't exist
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS user_selections (
+        id SERIAL PRIMARY KEY,
+        left_image_link VARCHAR,
+        right_image_link VARCHAR,
+        selected_image_link VARCHAR,
+        wallet_address VARCHAR,
+        timestamp TIMESTAMP
+    );
+''')
+conn.commit()
 
 def get_random_image_pair(df) -> Tuple[str, str]:
     left_image = random.choice(df['image_link'])
@@ -43,22 +56,22 @@ def show_image_pair(left_image: str, right_image: str, df):
         if st.button(label=df.loc[df['image_link'] == left_image, 'name'].iloc[0], key='left_button'):
             left_image, right_image = get_random_image_pair(df)
             show_image_pair(left_image, right_image, df)
-            # store_user_selection(left_image, right_image, left_image, wallet_address)
+            store_user_selection(left_image, right_image, left_image, wallet_address)
 
     with col4:
         if st.button(label=df.loc[df['image_link'] == right_image, 'name'].iloc[0], key='right_button'):
             left_image, right_image = get_random_image_pair(df)
             show_image_pair(left_image, right_image, df)
-            # store_user_selection(left_image, right_image, right_image, wallet_address)
+            store_user_selection(left_image, right_image, right_image, wallet_address)
 
-# def store_user_selection(left_image: str, right_image: str, selected_image: str, wallet_address: str):
+def store_user_selection(left_image: str, right_image: str, selected_image: str, wallet_address: str):
     # Insert user selection into the user_selections table
-  #  insert_query = sql.SQL('''
-   #     INSERT INTO user_selections (left_image_link, right_image_link, selected_image_link, wallet_address, timestamp)
-    #    VALUES (%s, %s, %s, %s, CURRENT_TIMESTAMP);
-    #''')
-    #cur.execute(insert_query, (left_image, right_image, selected_image, wallet_address))
-    #conn.commit()
+    insert_query = sql.SQL('''
+        INSERT INTO user_selections (left_image_link, right_image_link, selected_image_link, wallet_address, timestamp)
+        VALUES (%s, %s, %s, %s, CURRENT_TIMESTAMP);
+    ''')
+    cur.execute(insert_query, (left_image, right_image, selected_image, wallet_address))
+    conn.commit()
 
 def main(df):
     st.title("Nokiamon ELO Rating")
@@ -69,5 +82,12 @@ def main(df):
 
     show_image_pair(left_image, right_image, df)
 
+    # Store user selections and wallet addresses in the database
+    store_user_selection(left_image, right_image, None, wallet_address)
+
     # You can enhance this implementation by adding user authentication,
-    # tracking user selections, and calculating
+    # tracking user selections, and calculating the ELO rating for each Nokiamon.
+    # To do that, you'll need to store user selections and ELO ratings in a database.
+
+if __name__ == "__main__":
+    main(final_df)
