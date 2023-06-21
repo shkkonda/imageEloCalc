7db2cb46bd1546094e7ad2b7bca44a07f54c5b43
Shriharsha Konda <shkkonda@gmail.com>
2023-06-21 22:52:32 +0530

    removed database

--- app.py
+++ app.py
@@ -2,35 +2,11 @@ import random
 from typing import List, Tuple
 import streamlit as st
 import pandas as pd
-import psycopg2
-from psycopg2 import sql
 
 CSV_URL = "https://raw.githubusercontent.com/shkkonda/imageEloCalc/main/nokiamon_image.csv"
 final_df = pd.read_csv(CSV_URL)
 
-# Database connection details
-host = 'database-1.cv9g4hhrgmvg.us-east-1.rds.amazonaws.com'
-dbname = ''  # Update with your database name
-user = 'postgres'
-port = '5432'
-password = 'wCJTQ205EKHWh6fXzxLc'
-
-# Connect to the database
-conn = psycopg2.connect(host=host, dbname=dbname, user=user, port=port, password=password)
-cur = conn.cursor()
-
-# Create user_selections table if it doesn't exist
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS user_selections (
-        id SERIAL PRIMARY KEY,
-        left_image_link VARCHAR,
-        right_image_link VARCHAR,
-        selected_image_link VARCHAR,
-        wallet_address VARCHAR,
-        timestamp TIMESTAMP
-    );
-''')
-conn.commit()
+selected_data = []
 
 def get_random_image_pair(df) -> Tuple[str, str]:
     left_image = random.choice(df['image_link'])
@@ -55,33 +31,25 @@ def show_image_pair(left_image: str, right_image: str, df, wallet_address: str):
     if st.button(label=df.loc[df['image_link'] == left_image, 'name'].iloc[0], key=f'left_button_{left_image}'):
         left_image, right_image = get_random_image_pair(df)
         store_user_selection(left_image, right_image, left_image, wallet_address)
-        display_stored_selections()
+        show_image_pair(left_image, right_image, df, wallet_address)
 
     if st.button(label=df.loc[df['image_link'] == right_image, 'name'].iloc[0], key=f'right_button_{right_image}'):
         left_image, right_image = get_random_image_pair(df)
         store_user_selection(left_image, right_image, right_image, wallet_address)
-        display_stored_selections()
+        show_image_pair(left_image, right_image, df, wallet_address)
 
 def store_user_selection(left_image: str, right_image: str, selected_image: str, wallet_address: str):
-    # Insert user selection into the user_selections table
-    insert_query = sql.SQL('''
-        INSERT INTO user_selections (left_image_link, right_image_link, selected_image_link, wallet_address, timestamp)
-        VALUES (%s, %s, %s, %s, CURRENT_TIMESTAMP);
-    ''')
-    try:
-        cur.execute(insert_query, (left_image, right_image, selected_image, wallet_address))
-        conn.commit()  # Commit the changes to the database
-    except Exception as e:
-        st.write(f"Error storing user selection: {str(e)}")
+    selected_data.append({
+        'Left Image': left_image,
+        'Right Image': right_image,
+        'Selected Image': selected_image,
+        'Wallet Address': wallet_address,
+    })
 
 def display_stored_selections():
-    # Retrieve stored selections from the user_selections table
-    cur.execute("SELECT * FROM user_selections;")
-    selections = cur.fetchall()
-
-    if len(selections) > 0:
+    if selected_data:
+        df = pd.DataFrame(selected_data)
         st.subheader("Stored Selections")
-        df = pd.DataFrame(selections, columns=['ID', 'Left Image', 'Right Image', 'Selected Image', 'Wallet Address', 'Timestamp'])
         st.dataframe(df)
     else:
         st.write("No stored selections yet.")
@@ -94,11 +62,7 @@ def main(df):
     left_image, right_image = get_random_image_pair(df)
 
     show_image_pair(left_image, right_image, df, wallet_address)
-    display_stored_selections()  # Display the stored selections
-
-    # You can enhance this implementation by adding user authentication,
-    # tracking user selections, and calculating the ELO rating for each Nokiamon.
-    # To do that, you'll need to store user selections and ELO ratings in a database.
+    display_stored_selections()
 
 if __name__ == "__main__":
     main(final_df)
