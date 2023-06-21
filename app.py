b4095a09986f068ba28ffaa21df780b67f4f7419
Shriharsha Konda <shkkonda@gmail.com>
2023-06-21 23:24:32 +0530

    removed caching

--- app.py
+++ app.py
@@ -15,15 +15,6 @@ user = 'postgres'
 port = '5432'
 password = 'wCJTQ205EKHWh6fXzxLc'
 
-@st.cache(allow_output_mutation=True)
-def get_database_connection():
-    # Connect to the database
-    conn = psycopg2.connect(host=host, dbname=dbname, user=user, port=port, password=password)
-    return conn
-
-conn = get_database_connection()
-cur = conn.cursor()
-
 def get_random_image_pair(df) -> Tuple[str, str]:
     left_image = random.choice(df['image_link'])
     right_image = random.choice(df['image_link'])
@@ -55,6 +46,10 @@ def show_image_pair(left_image: str, right_image: str, df, wallet_address: str):
             store_user_selection(left_image, right_image, right_image, wallet_address)
 
 def store_user_selection(left_image: str, right_image: str, selected_image: str, wallet_address: str):
+    # Connect to the database
+    conn = psycopg2.connect(host=host, dbname=dbname, user=user, port=port, password=password)
+    cur = conn.cursor()
+
     # Insert user selection into the user_selections table
     insert_query = sql.SQL('''
         INSERT INTO user_selections (left_image_link, right_image_link, selected_image_link, wallet_address, timestamp)
@@ -62,6 +57,10 @@ def store_user_selection(left_image: str, right_image: str, selected_image: str,
     ''')
     cur.execute(insert_query, (left_image, right_image, selected_image, wallet_address))
     conn.commit()
+
+    # Close the database connection
+    cur.close()
+    conn.close()
 
 def main(df):
     st.title("Nokiamon ELO Rating")
